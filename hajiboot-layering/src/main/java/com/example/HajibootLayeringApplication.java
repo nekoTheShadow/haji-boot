f8ac0b2073c14791f09a0ef4057615a84f1cdf9d
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2018-12-31 16:02:40 +0900

    pp.41-45 [2.1.6] 「レイヤー化」した「コンポ―ネント」のインジェクション

---
+++ hajiboot-layering/src/main/java/com/example/HajibootLayeringApplication.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.domain.Customer;
+import com.example.service.CustomerService;
+
+@SpringBootApplication
+public class HajibootLayeringApplication implements CommandLineRunner {
+
+	@Autowired
+	CustomerService customerService;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		customerService.save(new Customer(1, "Nobita", "Nobi"));
+		customerService.save(new Customer(2, "Takashi", "Goda"));
+		customerService.save(new Customer(3, "Suneo", "Honekawa"));
+		customerService.findAll().forEach(System.out::println);
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(HajibootLayeringApplication.class, args);
+	}
+}
+
