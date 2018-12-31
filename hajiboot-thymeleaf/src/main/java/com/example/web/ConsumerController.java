89465c14a0a564d5a9a619a2a6a359617fc3ad3a
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2018-12-31 22:58:01 +0900

    pp.98-105 [3.3.2] 「顧客情報」の新規作成

--- hajiboot-thymeleaf/src/main/java/com/example/web/ConsumerController.java
+++ hajiboot-thymeleaf/src/main/java/com/example/web/ConsumerController.java
@@ -1,11 +1,17 @@
 package com.example.web;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Customer;
 import com.example.service.CustomerService;
 
 @Controller
@@ -15,10 +21,27 @@ public class ConsumerController {
 	@Autowired
 	CustomerService customerService;
 	
+	@ModelAttribute
+	CustomerForm setUpForm() {
+		return new CustomerForm();
+	}
+	
 	@GetMapping
 	String list(Model model) {
 		var customers = customerService.findAll();
 		model.addAttribute("customers", customers);
 		return "customers/list";
+	}
+	
+	@PostMapping(path="create")
+	String create(@Validated CustomerForm form, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return list(model);
+		}
+		
+		var customer = new Customer();
+		BeanUtils.copyProperties(form, customer);
+		customerService.create(customer);
+		return "redirect:/customers";
 	}
 }
