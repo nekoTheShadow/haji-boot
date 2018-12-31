f5ddaa392944a228ab3f9ac41dd39eeb3f1954c8
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2018-12-31 17:13:23 +0900

    pp.65-69 [2.3.1] 「JPA」の「エンティティ・クラス」の作成; [2.3.2] 「Spring Data JPA」を使った「レポジトリ・クラス」の作成

---
+++ hajiboot-jpa/src/main/java/com/example/HajibootJpaApplication.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.domain.Customer;
+import com.example.repository.CustomerRepository;
+
+@SpringBootApplication
+public class HajibootJpaApplication implements CommandLineRunner {
+	public static void main(String[] args) {
+		SpringApplication.run(HajibootJpaApplication.class, args);
+	}
+	
+	@Autowired
+	CustomerRepository customerRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		var created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
+		System.out.println(created + " is created!");
+		customerRepository.findAll().forEach(System.out::println);
+	}
+}
+
