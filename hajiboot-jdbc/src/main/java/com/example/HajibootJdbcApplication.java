2663b58015ad10dc38028d6494e3881a562c69c5
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2018-12-31 16:16:47 +0900

    pp.50-53 [2.2.1]  「JdbcTemplate」を使ったDBアクセス (「JdbcTemplate」を使った「オブジェクト・マッピング」)

--- hajiboot-jdbc/src/main/java/com/example/HajibootJdbcApplication.java
+++ hajiboot-jdbc/src/main/java/com/example/HajibootJdbcApplication.java
@@ -1,11 +1,17 @@
 package com.example;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import com.example.domain.Customer;
 
 @SpringBootApplication
 public class HajibootJdbcApplication implements CommandLineRunner {
@@ -14,11 +20,15 @@ public class HajibootJdbcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var sql = "SELECT :a + :b";
-		var param = new MapSqlParameterSource().addValue("a", 100).addValue("b", 200);
-		var result = jdbcTemplate.queryForObject(sql, param, Integer.class);
-		System.out.println("result = " + result);
-	}
+		var sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
+		var param = new MapSqlParameterSource().addValue("id", 1);
+		var result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
+			@Override
+			public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
+			}
+		});
+		System.out.println("result = " + result);	}
 		
 	public static void main(String[] args) {
 		SpringApplication.run(HajibootJdbcApplication.class, args);
