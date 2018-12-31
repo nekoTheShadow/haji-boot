2baa2b40989e6f06aa2c7b918d5865e73ff9c988
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2018-12-31 16:31:15 +0900

    pp.57-61 [2.2.4] 「JDBCTemplate」を使った「レポジトリ・クラスの実装」

--- hajiboot-jdbc/src/main/java/com/example/HajibootJdbcApplication.java
+++ hajiboot-jdbc/src/main/java/com/example/HajibootJdbcApplication.java
@@ -12,23 +12,19 @@ import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import com.example.domain.Customer;
+import com.example.repository.CustomerRepository;
 
 @SpringBootApplication
 public class HajibootJdbcApplication implements CommandLineRunner {
 	@Autowired
-	NamedParameterJdbcTemplate jdbcTemplate;
+	CustomerRepository customerRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		var sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
-		var param = new MapSqlParameterSource().addValue("id", 1);
-		var result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
-			@Override
-			public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
-				return new Customer(rs.getInt("id"), rs.getString("first_name"), rs.getString("last_name"));
-			}
-		});
-		System.out.println("result = " + result);	}
+		var created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
+		System.out.println(created + " is created!");
+		customerRepository.findAll().forEach(System.out::println);
+	}
 		
 	public static void main(String[] args) {
 		SpringApplication.run(HajibootJdbcApplication.class, args);
