e390bd92cd86de500d61642d8c47eeb47fb3fd45
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2019-01-04 22:46:12 +0900

    pp.173-175 [5.1] 「Hello World」アプリの結合テスト

--- hajiboot/src/test/java/com/example/HajibootApplicationTests.java
+++ hajiboot/src/test/java/com/example/HajibootApplicationTests.java
@@ -1,16 +1,31 @@
 package com.example;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HajibootApplicationTests {
-
+	@LocalServerPort
+	int port;
+	
+	@Autowired
+	TestRestTemplate restTemplate;
+	
 	@Test
 	public void contextLoads() {
+		var url = "http://localhost:" + port;
+		var response = restTemplate.getForEntity(url, String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).isEqualTo("Hello World Boot!");
 	}
 
 }
