46559d4fbcd6aa4bf0f3ed4dc7e20cadd45a7ac5
Hajime NAKAMURA <h.nakamura0903@gmail.com>
2019-01-04 22:17:08 +0900

    pp.170-171 [4.6.5] Spring Cloud Connectorsのコネクションプールに関する設定

--- hajiboot-rest/src/main/java/com/example/CloudConfig.java
+++ hajiboot-rest/src/main/java/com/example/CloudConfig.java
@@ -3,6 +3,8 @@ package com.example;
 import javax.sql.DataSource;
 
 import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.cloud.service.PooledServiceConnectorConfig;
+import org.springframework.cloud.service.relational.DataSourceConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,6 +15,8 @@ import org.springframework.context.annotation.Profile;
 public class CloudConfig extends AbstractCloudConfig {
 	@Bean
 	DataSource datasouce() {
-		return connectionFactory().dataSource();
+		var poolConfig = new PooledServiceConnectorConfig.PoolConfig(5, 30, 300);
+		var config = new DataSourceConfig(poolConfig, null);
+		return connectionFactory().dataSource(config);
 	}
 }
